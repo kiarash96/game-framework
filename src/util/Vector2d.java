aea1291d97ecb13632fe5b973e0a1ea72b642a0b
Kiarash Korki <kiarash.96@gmail.com>
2016-07-18 12:32:31 +0430

    Create a basic vector2d class

---
+++ src/util/Vector2d.java
@@ -0,0 +1,29 @@
+package util;
+
+/**
+ * Created by kiarash on 7/18/16.
+ */
+public class Vector2d {
+    double x, y;
+
+    public Vector2d(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
